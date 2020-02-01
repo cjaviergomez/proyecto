03f2ab1c831b27a6959a06ade7ed28105773cdaa
cjaviergomez <carlosjavier_960214@hotmail.com>
2020-01-31 21:32:49 -0500

    resolved routerlink error

--- src/app/proceso/forms/remodelacion/myAddon.module.ts
+++ src/app/proceso/forms/remodelacion/myAddon.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { startNewProcessComponent } from './startNewProcess.component';
 import { approveDataTaskComponent } from './approveDataTask.component';
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   entryComponents: [startNewProcessComponent,approveDataTaskComponent],
   declarations: [startNewProcessComponent,approveDataTaskComponent],
-  imports: [FormsModule],
+  imports: [FormsModule, RouterModule],
   exports: [startNewProcessComponent,approveDataTaskComponent]
 })
 export class MyAddonModule {}
